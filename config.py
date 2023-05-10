791696ab77f69d9642dee0e03b7d225aff93a272
nandhux99 <122593121+nandhux99@users.noreply.github.com>
2023-05-10 15:36:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee-db1.caqql7yp67s6.us-east-1.rds.amazonaws.com"
+customhost = "employee.caqql7yp67s6.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
 customdb = "employee"
-custombucket = "employee-db-demo"
+custombucket = "myawsbucket10523"
 customregion = "us-east-1"
